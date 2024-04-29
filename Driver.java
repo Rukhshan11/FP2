24e698d39ff6842c974d588424f2ea7690433ffd
Timk2331 <tkotov2331@students.mc3.edu>
2024-04-29 15:24:10 -0400

    Updated Fp2
    Cleared up uneeded files

--- Driver.java
+++ Driver.java
@@ -1,10 +1,10 @@
 import javafx.application.Application;
 //import javafx.event.ActionEvent;
-//import javafx.fxml.FXML;
-//import javafx.fxml.FXMLLoader;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 //import javafx.fxml.Initializable;
-//import javafx.scene.Parent;
-//import javafx.scene.Scene;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 //import javafx.scene.control.Button;
 //import javafx.scene.control.Label;
 //import javafx.scene.control.RadioButton;
